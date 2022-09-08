8de969f7d2c1bb8ab866d99912ee381fb4f553bc
matteobnvl <matteobonneval19@gmail.com>
2022-09-08 08:52:07 +0200

    deploiement

--- AppWorkShop/AppWorkShop/settings.py
+++ AppWorkShop/AppWorkShop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p7gbt1&_ho%tp1c5pc$w161907^kk-y=g-w_)ep++(skruop+m'
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+env = environ.Env()
+environ.Env.read_env(env_file=str(BASE_DIR / "AppWorkShop" / ".env"))
 
-ALLOWED_HOSTS = []
+
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env.bool("DEBUG")
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
